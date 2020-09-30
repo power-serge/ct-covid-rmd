f05af805496c9bc2cc27daa4786b994fba214a64
power-serge <sy120687@hotmail.com>
2020-09-29 20:26:30 -0400

    A(2): Template.Rmd, BuildCounties.R. These are the original files renamed.

--- RScripts/BuildCounties.R
+++ RScripts/BuildCounties.R
@@ -4,43 +4,35 @@
 library(readr)
 library(dplyr)
 
-# reference to data and html dirs and extension value
-datadir <- "../data"
-htmldir <- "../html"
-ext     <- ".html"
-
-# create the data directory if it doesn't exist
-if (!file.exists(datadir)){
-  dir.create(datadir)
-}
-
-# create the html directory if it doesn't exist
-if (!file.exists(htmldir)){
-  dir.create(htmldir)
-}
-
 # reference to the markdown template
 template  <- "../RMarkdown/Template.Rmd"
+
+# reference to output directory and extension
+htmldir <- "../html/"
+ext <- ".html"
 
 # get the data
 covid <- read_csv("https://data.ct.gov/resource/bfnu-rgqt.csv")
 
+# save data in a file
+save(covid, file = "../data/covid.RData")
+
 # make sure data is in the right order
 covid <- covid %>% arrange(dateupdated)
 
-# save data in a file
-save(covid, file = paste(datadir, "/covid.RData", sep = ""))
-
 # get distinct counties
 counties <- unique(covid$county)
+#counties <- c("New London", "New Haven")
 
 for (i in counties) {
   
   # build the file name and path
-  htmlfile <- paste(htmldir, "/", i, ext, sep="")
+  htmlfile <- paste(htmldir, i, ext, sep="")
   
   #print(htmlfile)
   
   # render the document
-  rmarkdown::render(template, output_file = htmlfile, params = list(data = i))
+  rmarkdown::render(template, output_file = htmlfile, params = list(data = i), quiet = TRUE)
 }
+
+
